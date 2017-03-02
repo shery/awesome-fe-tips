338e9493468aff2c52e307e6f99b3b6fbd4859f6
shery15 <382254994@qq.com>
2017-03-02 15:06:21 +0800

    update currying

--- currying.js
+++ currying.js
@@ -18,3 +18,21 @@ function add() {
 	};
 	return addInner();
 }
+/**
+ * 柯里化函数 这里要实现的功能 将尾递归函数 tailFactorial 变为只接受1个参数的 factorial
+ * @param  {Function} fn 待处理函数，这里对应 tailFactorial 函数
+ * @param  {[type]}   n  其中一个参数默认值，这里 total 默认值定为 1
+ * @return {[type]}      [description]
+ */
+function currying(fn, n) {
+  return function (m) {
+    return fn.call(this, m, n);
+  };
+}
+
+function tailFactorial(n, total) {
+  if (n === 1) return total;
+  return tailFactorial(n - 1, n * total);
+}
+
+const factorial = currying(tailFactorial, 1);
