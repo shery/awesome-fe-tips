f4299906e857238c3c152f7f48cc513944f52f1e
shery15 <382254994@qq.com>
2017-03-24 23:37:00 +0800

    upadte

--- array.js
+++ array.js
@@ -2,3 +2,15 @@
  * 去除数组的重复成员
  */
 [...new Set(array)]
+
+let unique = function (array) {
+    let ro = {};
+    let ra = [];
+    array.forEach(item=>{
+        if(!ro[item]){
+            ro[item] = item;
+            ra.push(item);
+        }
+    });
+    return ra;
+}
