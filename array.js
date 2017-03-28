d6fcb5bc5fb2f4b7bb2658f5d2b9bb6bd4e79ab6
shery15 <382254994@qq.com>
2017-03-28 19:56:51 +0800

    update

--- array.js
+++ array.js
@@ -14,3 +14,12 @@ let unique = function (array) {
     });
     return ra;
 }
+
+/**
+ * 合并数组
+ */
+let arrA = [1, 2, 3];
+let arrB = [4, 5, 6];
+
+// 将 arrB 融合进 arrA, 改变原数组 arrA
+[].push.apply(arrA, arrB);
