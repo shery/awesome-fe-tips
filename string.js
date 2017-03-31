220b6f384ccaae2102a03081442c93c573810d47
shery15 <382254994@qq.com>
2017-03-31 23:06:22 +0800

    update

--- string.js
+++ string.js
@@ -10,3 +10,7 @@ let str_to_array = function(str){
 let str_to_array = function (str) {
   return Array.prototype.map.call(str,function(x){return x});
 }
+
+let checkPalindrom = function(str){
+  return str_to_array(str).reverse().join('');
+}
