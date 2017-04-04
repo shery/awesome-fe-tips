4347e0917ac4bba799d380ad20bd5698d55850f7
shery15 <382254994@qq.com>
2017-04-04 23:09:28 +0800

    UPDATE

--- string.js
+++ string.js
@@ -1,5 +1,9 @@
 /**
- * 将字符串拆分成数组
+ * 判断一个字符串是不是回文
+ */
+
+/**
+ * step: 1 将字符串拆分成数组
  * @param  {[type]} str [description]
  * @return {[type]}     [description]
  */
@@ -7,15 +11,29 @@ let str_to_array = function(str){
   return str.split('');
 }
 
+/**
+ * step: 1* 将字符串拆分成数组
+ * @param  {[type]} str [description]
+ * @return {[type]}     [description]
+ */
 let str_to_array = function (str) {
-  return Array.prototype.map.call(str,function(x){return x});
+  return Array.prototype.map.call(str,function(x) { return x });
 }
 
+/**
+ * step：2 检查是不是回文
+ * @param  {[type]} str [description]
+ * @return {[type]}     [description]
+ */
 let checkPalindrom = function(str){
-  return str_to_array(str).reverse().join('');
+  return str == str_to_array(str).reverse().join('');
 }
 
-
+/**
+ * 利用数组的reduceRight()方法 实现
+ * @param  {[type]} str [description]
+ * @return {[type]}     [description]
+ */
 let rs = (str) =>{
     return Array.prototype.reduceRight.apply(str,[function(pre,current){
         return pre + current;
