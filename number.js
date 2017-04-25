0020cad41304354964f7b2d8f1018d0aecb44ca8
shery <382254994@qq.com>
2017-04-25 23:58:03 +0800

    Create number.js

---
+++ number.js
@@ -0,0 +1,18 @@
+/**
+ * 浮点数运算修正 -- 乘法
+ * @param  {[type]} a [description]
+ * @param  {[type]} b [description]
+ * @return {[type]}   [description]
+ */
+function multiply(a, b) {
+    var c = 0,
+        d = a.toString(),
+        e = b.toString();
+    try {
+        c += d.split(".")[1].length;
+    } catch (f) {}
+    try {
+        c += e.split(".")[1].length;
+    } catch (f) {}
+    return Number(d.replace(".", "")) * Number(e.replace(".", "")) / Math.pow(10, c);
+}
