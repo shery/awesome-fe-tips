c948856e32bf039f61835b7df553d1a0c729593c
shery <382254994@qq.com>
2017-04-26 23:16:18 +0800

    Update number.js

--- number.js
+++ number.js
@@ -1,4 +1,25 @@
 /**
+ * 浮点数运算修正 -- 加法
+ * @param  {[type]} a [description]
+ * @param  {[type]} b [description]
+ * @return {[type]}   [description]
+ */
+function add(a, b) {
+    var c, d, e;
+    try {
+        c = a.toString().split(".")[1].length;
+    } catch (f) {
+        c = 0;
+    }
+    try {
+        d = b.toString().split(".")[1].length;
+    } catch (f) {
+        d = 0;
+    }
+    return e = Math.pow(10, Math.max(c, d)), (mul(a, e) + mul(b, e)) / e;
+}
+
+/**
  * 浮点数运算修正 -- 乘法
  * @param  {[type]} a [description]
  * @param  {[type]} b [description]
