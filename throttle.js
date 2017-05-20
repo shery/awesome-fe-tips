0a47f4255dfda080ca7ecf28f102f6b607948433
shery <382254994@qq.com>
2017-05-21 07:16:46 +0800

    Create throttle.js

---
+++ throttle.js
@@ -0,0 +1,11 @@
+function throttle(callback, delay) {
+    var previousCall = new Date().getTime();
+    return function() {
+      var time = new Date().getTime();
+
+      if ((time - previousCall) >= delay) {
+        previousCall = time;
+        callback.apply(null, arguments);
+      }
+    };
+  }
